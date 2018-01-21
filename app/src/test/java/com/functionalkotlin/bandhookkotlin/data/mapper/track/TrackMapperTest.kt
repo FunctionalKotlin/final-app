5b69aae7ee3fbf4492265e90cddb354bb4efffdc
Alejandro Hernández <info@alejandrohdezma.com>
2018-01-21 17:20:00 +0100

    Use KotlinTest in TrackMapper

--- app/src/test/java/com/functionalkotlin/bandhookkotlin/data/mapper/track/TrackMapperTest.kt
+++ app/src/test/java/com/functionalkotlin/bandhookkotlin/data/mapper/track/TrackMapperTest.kt
@@ -4,68 +4,46 @@ package com.functionalkotlin.bandhookkotlin.data.mapper.track
 
 import com.functionalkotlin.bandhookkotlin.data.lastfm.model.LastFmArtist
 import com.functionalkotlin.bandhookkotlin.data.lastfm.model.LastFmTrack
+import io.kotlintest.matchers.haveSize
+import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
-import org.junit.Before
-import org.junit.Test
 
-class TrackMapperTest {
+class TrackMapperTest: StringSpec() {
+    init {
+        val lastFmArtist = LastFmArtist("name", "mbid", "url", emptyList(), null, null)
+        val lastFmTrack = LastFmTrack("name", 10, null, "url", lastFmArtist)
 
-    lateinit var lastFmTracks: List<LastFmTrack>
-    lateinit var lastFmTrack: LastFmTrack
-    lateinit var lastFmArtist: LastFmArtist
+        val trackMapper = TrackMapper()
 
-    lateinit var trackMapper: TrackMapper
+        "transform valid tracks return valid list" {
+            val tracks = trackMapper.transform(listOf(lastFmTrack, lastFmTrack))
 
-    @Before
-    fun setUp() {
-        lastFmArtist = LastFmArtist("artist name", "artist mbid", "artist url", emptyList(), null, null)
-        lastFmTrack = LastFmTrack("track name", 10, null, "track url", lastFmArtist)
-        lastFmTracks = listOf(lastFmTrack, lastFmTrack)
+            tracks should haveSize(2)
+            lastFmTrack.run {
+                tracks[0].name shouldBe name
+                tracks[0].duration shouldBe duration
+                tracks[1].name shouldBe name
+                tracks[1].duration shouldBe duration
+            }
+        }
 
-        trackMapper = TrackMapper()
-    }
+        "transform null list returns empty list" {
+            trackMapper.transform(null) should haveSize(0)
+        }
 
-    @Test
-    fun testTransformTracks() {
-        // When
-        val tracks = trackMapper.transform(lastFmTracks)
+        "transform empty list returns empty list" {
+            trackMapper.transform(emptyList()) should haveSize(0)
+        }
 
-        // Then
-        assertEquals(2, tracks.size)
-        assertEquals(lastFmTracks[0].name, tracks[0].name)
-        assertEquals(lastFmTracks[0].duration, tracks[0].duration)
-        assertEquals(lastFmTracks[1].name, tracks[1].name)
-        assertEquals(lastFmTracks[1].duration, tracks[1].duration)
-    }
-
-    @Test
-    fun testTransformTracks_null() {
-        // When
-        val tracks = trackMapper.transform(null)
-
-        // Then
-        assertTrue(tracks.isEmpty())
-    }
-
-    @Test
-    fun testTransformTracks_empty() {
-        // When
-        val tracks = trackMapper.transform(emptyList())
-
-        // Then
-        assertTrue(tracks.isEmpty())
-    }
-
-    @Test
-    fun testTransformTrack() {
-        // When
-        val track = trackMapper.transform(lastFmTrack)
-
-        // Then
-        assertNotNull(track)
-        assertEquals(lastFmTrack.name, track.name)
-        assertEquals(lastFmTrack.duration, track.duration)
+        "transform track returns track" {
+            trackMapper.transform(lastFmTrack).run {
+                name shouldBe lastFmTrack.name
+                duration shouldBe lastFmTrack.duration
+            }
+        }
     }
 }
