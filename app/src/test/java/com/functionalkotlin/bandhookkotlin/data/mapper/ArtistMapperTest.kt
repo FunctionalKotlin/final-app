6937725831d372ae221a5e396550209e97605a3d
Alejandro Hernández <info@alejandrohdezma.com>
2018-01-21 17:20:00 +0100

    Use KotlinTest in ArtistMapper

--- app/src/test/java/com/functionalkotlin/bandhookkotlin/data/mapper/ArtistMapperTest.kt
+++ app/src/test/java/com/functionalkotlin/bandhookkotlin/data/mapper/ArtistMapperTest.kt
@@ -6,84 +6,72 @@ import com.functionalkotlin.bandhookkotlin.data.lastfm.model.LastFmArtist
 import com.functionalkotlin.bandhookkotlin.data.lastfm.model.LastFmBio
 import com.functionalkotlin.bandhookkotlin.data.lastfm.model.LastFmImage
 import com.functionalkotlin.bandhookkotlin.data.lastfm.model.LastFmImageType
+import io.kotlintest.matchers.haveSize
+import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldNotBe
+import io.kotlintest.specs.StringSpec
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
 
-class ArtistMapperTest {
-    lateinit var artistMapper: ArtistMapper
-    lateinit var artistsList: List<LastFmArtist>
-    lateinit var validLastFmArtistWithMegaImage: LastFmArtist
-    lateinit var validLastFmArtistWithoutMegaImage: LastFmArtist
-    lateinit var invalidLastFmArtist: LastFmArtist
-    lateinit var megaLastFmImage: LastFmImage
-    lateinit var smallLastFmImage: LastFmImage
+class ArtistMapperTest: StringSpec() {
+    init {
+        val coldplayBio = LastFmBio("British rock band formed in 1996")
+        val megaImageUrl = "megaImageOneUrl"
+        val smallImageUrl = "smallImageOneUrl"
 
-    private val coldplayBio = LastFmBio("British rock band formed in 1996")
-    private val megaImageUrl = "megaImageOneUrl"
-    private val smallImageUrl = "smallImageOneUrl"
+        val megaLastFmImage = LastFmImage(megaImageUrl, LastFmImageType.MEGA.type)
+        val smallLastFmImage = LastFmImage(smallImageUrl, LastFmImageType.SMALL.type)
 
-    @Before
-    fun setUp() {
-        megaLastFmImage = LastFmImage(megaImageUrl, LastFmImageType.MEGA.type)
-        smallLastFmImage = LastFmImage(smallImageUrl, LastFmImageType.SMALL.type)
+        val validLastFmArtistWithMegaImage = LastFmArtist(
+            "Coldplay", "mbid", "url", listOf(megaLastFmImage, smallLastFmImage), null,
+            coldplayBio)
+        val validLastFmArtistWithoutMegaImage = LastFmArtist(
+            "Him", "mbid", "url", listOf(smallLastFmImage, smallLastFmImage), null, null)
+        val invalidLastFmArtist = LastFmArtist(
+            "Unknown", null, "url", listOf(megaLastFmImage, smallLastFmImage), null, null)
 
-        validLastFmArtistWithMegaImage = LastFmArtist("Coldplay", "mbid", "someurl",
-                listOf(megaLastFmImage, smallLastFmImage), null, coldplayBio)
-        validLastFmArtistWithoutMegaImage = LastFmArtist("Him", "mbid", "someurl",
-                listOf(smallLastFmImage, smallLastFmImage), null, null)
-        invalidLastFmArtist = LastFmArtist("Unknown", null, "someurl",
-                listOf(megaLastFmImage, smallLastFmImage), null, null)
+        val artistsList = listOf(validLastFmArtistWithMegaImage,
+            invalidLastFmArtist,
+            validLastFmArtistWithoutMegaImage)
 
-        artistsList = listOf(validLastFmArtistWithMegaImage,
-                             invalidLastFmArtist,
-                             validLastFmArtistWithoutMegaImage)
+        val artistMapper = ArtistMapper()
 
-        artistMapper = ArtistMapper()
+        "transform valid list returns artist list" {
+            val artists = artistMapper.transform(artistsList)
+
+            artists should haveSize(2)
+            artists[0].id shouldBe validLastFmArtistWithMegaImage.mbid
+            artists[1].id shouldBe validLastFmArtistWithMegaImage.mbid
+        }
+
+        "transform artist with mega image return valid artist" {
+            val artist = artistMapper.transform(validLastFmArtistWithMegaImage)
+
+            artist?.run {
+                id shouldBe validLastFmArtistWithMegaImage.mbid
+                name shouldBe validLastFmArtistWithMegaImage.name
+                bio shouldBe validLastFmArtistWithMegaImage.bio?.content
+                url shouldBe megaImageUrl
+            }
+        }
+
+        "transform artist without mega image return valid artist" {
+            val artist = artistMapper.transform(validLastFmArtistWithoutMegaImage)
+
+            artist?.run {
+                id shouldBe validLastFmArtistWithoutMegaImage.mbid
+                name shouldBe validLastFmArtistWithoutMegaImage.name
+                bio shouldBe validLastFmArtistWithoutMegaImage.bio?.content
+                url shouldBe smallImageUrl
+            }
+        }
+
+        "transform invalid artist returns null" {
+            artistMapper.transform(invalidLastFmArtist) shouldBe null
+        }
     }
 
-    @Test
-    fun testTransformArtists() {
-        // When
-        val artists = artistMapper.transform(artistsList)
-
-        // Then
-        assertEquals(2, artists.size)
-        assertEquals(validLastFmArtistWithMegaImage.mbid, artists[0].id)
-        assertEquals(validLastFmArtistWithoutMegaImage.mbid, artists[1].id)
-    }
-
-    @Test
-    fun testTransformArtist_ValidArtistWithMegaImage() {
-        // When
-        val artist = artistMapper.transform(validLastFmArtistWithMegaImage)
-
-        // Then
-        assertEquals(validLastFmArtistWithMegaImage.mbid, artist?.id)
-        assertEquals(validLastFmArtistWithMegaImage.name, artist?.name)
-        assertEquals(validLastFmArtistWithMegaImage.bio?.content, artist?.bio)
-        assertEquals(megaImageUrl, artist?.url)
-    }
-
-    @Test
-    fun testTransformArtist_ValidArtistWithoutMegaImage() {
-        // When
-        val artist = artistMapper.transform(validLastFmArtistWithoutMegaImage)
-
-        // Then
-        assertEquals(validLastFmArtistWithoutMegaImage.mbid, artist?.id)
-        assertEquals(validLastFmArtistWithoutMegaImage.name, artist?.name)
-        assertEquals(validLastFmArtistWithoutMegaImage.bio?.content, artist?.bio)
-        assertEquals(smallImageUrl, artist?.url)
-    }
-
-    @Test
-    fun testTransformArtist_InvalidArtist() {
-        // When
-        val artist = artistMapper.transform(invalidLastFmArtist)
-
-        // Then
-        assertNull(artist)
-    }
 }
